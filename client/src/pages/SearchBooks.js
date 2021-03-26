0ed2d3a7a062a3e1f0d19111690d059f9ed7453d
Nick Hill <nickhill2380@gmail.com>
2021-03-25 21:56:22 -0400

    updeated searchbook page

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
-import { saveBook, searchGoogleBooks } from '../utils/API';
+import { searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/react-hooks';
+import { SAVE_BOOK } from '../utils/queries';
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -13,6 +15,7 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -65,7 +68,9 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables: {...savedBookIds}
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
@@ -102,6 +107,7 @@ const SearchBooks = () => {
               </Col>
             </Form.Row>
           </Form>
+          {error && <div>Search Failed</div>}
         </Container>
       </Jumbotron>
 
